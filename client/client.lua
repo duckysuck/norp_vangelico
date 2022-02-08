003cb6c1681bc07e2a745342a26d7017d4eed1c3
Vumono <87247208+Vumono@users.noreply.github.com>
2022-02-08 12:22:10 +0100

    preparation of rayfire usage

--- client/client.lua
+++ client/client.lua
@@ -211,27 +211,27 @@ function loadAnimDict(dict)
 end
 
 local showcases = {
-	{x = 147.085, y = -1048.612, z = 29.346, heading = 70.326, isOpen = false},--
-	{x = -626.735, y = -238.545, z = 38.057, heading = 214.907, isOpen = false},--
-	{x = -625.697, y = -237.877, z = 38.057, heading = 217.311, isOpen = false},--
-	{x = -626.825, y = -235.347, z = 38.057, heading = 33.745, isOpen = false},--
-	{x = -625.77, y = -234.563, z = 38.057, heading = 33.572, isOpen = false},--
-	{x = -627.957, y = -233.918, z = 38.057, heading = 215.214, isOpen = false},--
-	{x = -626.971, y = -233.134, z = 38.057, heading = 215.532, isOpen = false},--
-	{x = -624.433, y = -231.161, z = 38.057, heading = 305.159, isOpen = false},--
-	{x = -623.045, y = -232.969, z = 38.057, heading = 303.496, isOpen = false},--
-	{x = -620.265, y = -234.502, z = 38.057, heading = 217.504, isOpen = false},--
-	{x = -619.225, y = -233.677, z = 38.057, heading = 213.35, isOpen = false},--
-	{x = -620.025, y = -233.354, z = 38.057, heading = 34.18, isOpen = false},--
-	{x = -617.487, y = -230.605, z = 38.057, heading = 309.177, isOpen = false},--
-	{x = -618.304, y = -229.481, z = 38.057, heading = 304.243, isOpen = false},--
-	{x = -619.741, y = -230.32, z = 38.057, heading = 124.283, isOpen = false},--
-	{x = -619.69, y = -227.61, z = 38.057, heading = 305.245, isOpen = false},--
-	{x = -620.481, y = -226.59, z = 38.057, heading = 304.677, isOpen = false},--
-	{x = -621.098, y = -228.495, z = 38.057, heading = 127.046, isOpen = false},--
-	{x = -623.855, y = -227.051, z = 38.057, heading = 38.605, isOpen = false},--
-	{x = -624.977, y = -227.884, z = 38.057, heading = 48.847, isOpen = false},--
-	{x = -624.056, y = -228.228, z = 38.057, heading = 216.443, isOpen = false},--
+	{x = 147.085, y = -1048.612, z = 29.346, heading = 70.326, isOpen = false, rayfire = "DES_Jewel_Cab"},--
+	{x = -626.735, y = -238.545, z = 38.057, heading = 214.907, isOpen = false, rayfire = "DES_Jewel_Cab3"},--
+	{x = -625.697, y = -237.877, z = 38.057, heading = 217.311, isOpen = false, rayfire = "DES_Jewel_Cab4"},--
+	{x = -626.825, y = -235.347, z = 38.057, heading = 33.745, isOpen = false, rayfire = "DES_Jewel_Cab"},--
+	{x = -625.77, y = -234.563, z = 38.057, heading = 33.572, isOpen = false, rayfire = "DES_Jewel_Cab2"},--
+	{x = -627.957, y = -233.918, z = 38.057, heading = 215.214, isOpen = false, rayfire = "DES_Jewel_Cab"},--
+	{x = -626.971, y = -233.134, z = 38.057, heading = 215.532, isOpen = false, rayfire = "DES_Jewel_Cab3"},--
+	{x = -624.433, y = -231.161, z = 38.057, heading = 305.159, isOpen = false, rayfire = "DES_Jewel_Cab3"},--
+	{x = -623.045, y = -232.969, z = 38.057, heading = 303.496, isOpen = false, rayfire = "DES_Jewel_Cab4"},--
+	{x = -620.265, y = -234.502, z = 38.057, heading = 217.504, isOpen = false, rayfire = "DES_Jewel_Cab"},--
+	{x = -619.225, y = -233.677, z = 38.057, heading = 213.35, isOpen = false, rayfire = "DES_Jewel_Cab2"},--
+	{x = -620.025, y = -233.354, z = 38.057, heading = 34.18, isOpen = false, rayfire = "DES_Jewel_Cab2"},--
+	{x = -617.487, y = -230.605, z = 38.057, heading = 309.177, isOpen = false, rayfire = "DES_Jewel_Cab3"},--
+	{x = -618.304, y = -229.481, z = 38.057, heading = 304.243, isOpen = false, rayfire = "DES_Jewel_Cab4"},--
+	{x = -619.741, y = -230.32, z = 38.057, heading = 124.283, isOpen = false, rayfire = "DES_Jewel_Cab"},--
+	{x = -619.69, y = -227.61, z = 38.057, heading = 305.245, isOpen = false, rayfire = "DES_Jewel_Cab"},--
+	{x = -620.481, y = -226.59, z = 38.057, heading = 304.677, isOpen = false, rayfire = "DES_Jewel_Cab2"},--
+	{x = -621.098, y = -228.495, z = 38.057, heading = 127.046, isOpen = false, rayfire = "DES_Jewel_Cab3"},--
+	{x = -623.855, y = -227.051, z = 38.057, heading = 38.605, isOpen = false, rayfire = "DES_Jewel_Cab"},--
+	{x = -624.977, y = -227.884, z = 38.057, heading = 48.847, isOpen = false, rayfire = "DES_Jewel_Cab4"},--
+	{x = -624.056, y = -228.228, z = 38.057, heading = 216.443, isOpen = false, rayfire = "DES_Jewel_Cab4"},--
 }
 
 function animation()
